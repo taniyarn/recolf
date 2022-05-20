5bf2a110805b09f1eade6320abbb9d7f9a169f67
ttomo.bb <ttomo.bb@gmail.com>
2022-05-20 20:57:41 +0900

    add

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,21 +1,76 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:recolf/models/video.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Future<List<Image>> genThumbnails() async {
+    final _images = <Image>[];
+    final videoBox = Hive.box<Video>('videoBox');
+
+    final box = videoBox.toMap();
+
+    for (final b in box.values) {
+      var _tempDir = '';
+      await getTemporaryDirectory().then((d) => _tempDir = d.path);
+      final thumbnailPath = await VideoThumbnail.thumbnailFile(
+        video: b.path,
+        headers: {
+          "USERHEADER1": "user defined header1",
+          "USERHEADER2": "user defined header2",
+        },
+        thumbnailPath: _tempDir,
+        imageFormat: ImageFormat.PNG,
+      );
+      print("thumbnail file is located: $thumbnailPath");
+
+      final file = File(thumbnailPath!);
+      final bytes = file.readAsBytesSync();
+      final _image = Image.memory(
+        bytes,
+        fit: BoxFit.cover,
+      );
+      _images.add(_image);
+    }
+
+    return _images;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final box = Hive.box<Video>('videoBox');
+    final videoBox = Hive.box<Video>('videoBox');
     return Scaffold(
-      body: ListView.builder(
-        itemCount: box.length,
-        itemBuilder: (context, listIndex) {
-          return ListTile(
-            title: Text(box.getAt(listIndex)!.path),
-          );
+      body: FutureBuilder<List<Image>>(
+        future: genThumbnails(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ValueListenableBuilder(
+              valueListenable: videoBox.listenable(),
+              builder: (context, Box<Video> box, _) {
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisSpacing: 4,
+                    crossAxisSpacing: 4,
+                    crossAxisCount: 4,
+                  ),
+                  padding: EdgeInsets.all(4),
+                  itemCount: box.length,
+                  itemBuilder: (context, listIndex) {
+                    return snapshot.data![listIndex];
+                  },
+                );
+              },
+            );
+          } else {
+            return const CircularProgressIndicator();
+          }
         },
       ),
       floatingActionButton: FloatingActionButton(
